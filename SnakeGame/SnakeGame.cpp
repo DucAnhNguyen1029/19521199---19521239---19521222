73b9fb4f16b3ec969560693d3c385a1db7e0d776
Đức Anh Nguyễn <ducanh102901@gmail.com>
2020-07-02 21:22:20 +0700

    submit 19521199

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -1,13 +1,49 @@
 
 #include <iostream>
+#include<windows.h>
 using namespace std;
 
+const int width = 20;
+const int length = 20;
+int x, y, food_X, food_Y, score;
+bool end_game;
+enum direction { Stop = 0, Left, Right, Up, Down };
+direction dir;
 
-
-int main()
+void New_snake()
 {
 
 
+}
 
+void DrawMap()
+{
+
+
+}
+
+void Key_input()
+{
+
+
+}
+
+
+void Rule()
+{
+
+
+}
+
+int main()
+{
+	New_snake();
+	while (!end_game)
+	{
+		DrawMap();
+		Key_input();
+		Rule();
+		Sleep(30);
+	}
     return 0;
 }
