a719938f5305f412c6e12c4085c8f5600285d38a
Đức Anh Nguyễn <ducanh102901@gmail.com>
2020-07-03 08:19:05 +0700

    submit 19521199

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -2,19 +2,26 @@
 #include <iostream>
 #include<windows.h>
 #include<conio.h>
+#include<ctime>
 using namespace std;
 
 const int width = 20;
 const int length = 20;
 int x, y, food_X, food_Y, score;
 bool end_game;
-enum direction { Stop = 0, Left, Right, Up, Down };
+enum direction { Stop = 0, Left, Up, Right, Down };
 direction dir;
 
 void New_snake()
 {
-
-
+	srand(time(NULL));
+	end_game = false;
+	score = 0;
+	dir = Stop;
+	x = length / 2;
+	y = width / 2;
+	food_X = rand() % ((length - 2) - 2 + 1) + 2;
+	food_Y = rand() % ((width - 2) - 2 + 1) + 2;
 }
 
 void DrawMap()
